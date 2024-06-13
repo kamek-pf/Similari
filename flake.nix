7b00ada38dd556dd572986c35925e0bfd69211e2
Kamek <b.kamek@gmail.com>
2024-06-13 10:31:54 -0400

    Add Nix flake, bump dependencies

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ cargo rustc rust-analyzer rustfmt cargo-outdated python3 ];
+        };
+      });
+}
